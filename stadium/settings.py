4522c0adcea0dacc851e380bfe5145f5a963b974
Tomas Šerpenskas <168423322+1nsomn1aa@users.noreply.github.com>
2025-05-06 15:03:59 +0100

    Rotate secret key and place it in environment variables

--- stadium/settings.py
+++ stadium/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9&)f4gjou7)g3f**c(0it(4efz0h%pizuhva9i@_sd6exb^(o*'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
